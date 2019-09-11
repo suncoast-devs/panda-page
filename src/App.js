7c0ef109163ada5dfaeaa0f65ba443266bda9f39
Jason L Perry <jason@suncoast.io>
2019-09-11 14:44:35 -0400

    Update
    A lazy commit.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,102 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
 
 class App extends Component {
   render() {
-    return <HelloWorld />
+    return (
+      <main>
+        <header>
+          <img
+            src="https://stickershop.line-scdn.net/stickershop/v1/product/1405071/LINEStorePC/main.png;compress=true"
+            alt=""
+          />
+          <h1>All About Pandas</h1>
+        </header>
+        <aside>
+          <figure>
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Grosser_Panda.JPG/440px-Grosser_Panda.JPG"
+              alt="Giant Panda"
+            />
+            <figcaption>Giant panda at the Ocean Park Hong Kong</figcaption>
+          </figure>
+        </aside>
+        <section>
+          <p>
+            The{' '}
+            <a href="https://en.wikipedia.org/wiki/Giant_panda">giant panda</a>{' '}
+            (Ailuropoda melanoleuca; Chinese: 大熊猫; pinyin: dà xióng māo),
+            also known as panda bear or simply panda, is a bear native to south
+            central China. It is easily recognized by the large, distinctive
+            black patches around its eyes, over the ears, and across its round
+            body. The name "giant panda" is sometimes used to distinguish it
+            from the red panda. Though it belongs to the order Carnivora, the
+            giant panda is a folivore, with bamboo shoots and leaves making up
+            more than 99% of its diet. Giant pandas in the wild will
+            occasionally eat other grasses, wild tubers, or even meat in the
+            form of birds, rodents, or carrion. In captivity, they may receive
+            honey, eggs, fish, yams, shrub leaves, oranges, or bananas along
+            with specially prepared food.
+          </p>
+          <p>
+            The giant panda lives in a few mountain ranges in central China,
+            mainly in Sichuan, but also in neighbouring Shaanxi and Gansu. As a
+            result of farming, deforestation, and other development, the giant
+            panda has been driven out of the lowland areas where it once lived.
+          </p>
+          <h2>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+            fuga nemo tenetur beatae dolorem iure repellendus, quod deleniti id
+            nam.
+          </h2>
+          <p>
+            The giant panda is a conservation-reliant vulnerable species. A 2007
+            report showed 239 pandas living in captivity inside China and
+            another 27 outside the country. As of December 2014, 49 giant pandas
+            lived in captivity outside China, living in 18 zoos in 13 different
+            countries. Wild population estimates vary; one estimate shows that
+            there are about 1,590 individuals living in the wild, while a 2006
+            study via DNA analysis estimated that this figure could be as high
+            as 2,000 to 3,000. Some reports also show that the number of giant
+            pandas in the wild is on the rise. In March 2015, conservation news
+            site Mongabay stated that the wild giant panda population had
+            increased by 268, or 16.8%, to 1,864. In 2016, the IUCN reclassified
+            the species from "endangered" to "vulnerable".
+          </p>
+          <blockquote>
+            <p>
+              While the dragon has often served as China's national symbol,
+              internationally the giant panda has often filled this role. As
+              such, it is becoming widely used within China in international
+              contexts, for example, appearing since 1982 on gold panda bullion
+              coins and as one of the five Fuwa mascots of the Beijing Olympics.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+              minus nesciunt necessitatibus expedita vel praesentium corporis,
+              culpa dolor nihil libero autem? Voluptates alias commodi
+              exercitationem, similique quo vero sed rem?
+            </p>
+          </blockquote>
+
+          <h2>Cold things about pandas</h2>
+          <ul>
+            <li>They poop a lot</li>
+            <li>Eat bamboo</li>
+            <li>
+              They fight to death over mates
+              <ul>
+                <li>It's someones job to make sure they procreate.</li>
+                <li>The female is in tree</li>
+              </ul>
+            </li>
+            <li>Pandas have enslaved China</li>
+          </ul>
+        </section>
+        <footer>
+          <p>Built with &hearts; @ SDG in St. Petersburg, Florida.</p>
+        </footer>
+      </main>
+    )
   }
 }
 
